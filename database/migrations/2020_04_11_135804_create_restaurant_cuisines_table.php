06f6e5dd25b717d038c770f9f78f5c5db4802ccc
etekumoses <etekemoses@gmail.com>
2020-10-22 17:04:03 +0300

    push

--- database/migrations/2020_04_11_135804_create_restaurant_cuisines_table.php
+++ database/migrations/2020_04_11_135804_create_restaurant_cuisines_table.php
@@ -29,6 +29,6 @@ class CreateRestaurantCuisinesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('restaurant_fields');
+        Schema::dropIfExists('restaurant_fields');
     }
 }
